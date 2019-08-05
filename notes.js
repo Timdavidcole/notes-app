175ef1d1c94673cf8f22a1f361c84691b8413f33
Kareem <kareem335@yahoo.co.uk>
2019-08-05 16:22:52 +0100

    added innerhtml

--- notes.js
+++ notes.js
@@ -1,8 +1,7 @@
 var noteList = function(array) {
   array.forEach(function(note){
-    var linebreak = document.createElement("br");
-    var textnode = document.createTextNode(note);
-    document.getElementById("notesList").appendChild(textnode)
-    document.getElementById("notesList").appendChild(linebreak)
+    var div = document.createElement('div')
+    div.innerHTML = note + "<br>"
+    document.body.appendChild(div);
   })
 }
