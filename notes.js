2ef3350045bf9b0aef6ff7f23f881468c0292597
timdavidcole <timdavidcole@gmail.com>
2019-08-05 16:42:16 +0100

    update list in progress

--- notes.js
+++ notes.js
@@ -1,7 +1,15 @@
 var noteList = function(array) {
-  array.forEach(function(note){
-    var div = document.createElement('div')
-    div.innerHTML = note + "<br>"
-    document.body.appendChild(div);
+  var list = document.getElementById("notesList")
+  console.log(list.childNodes)
+  // list.removeChild(list.childNodes)
+  array.forEach(function(note, index){
+    var div = document.createElement('div');
+    var br = document.createElement('br');
+    div.innerHTML = note;
+    div.setAttribute("id", "note_" + index);
+    div.setAttribute("class", "note_small");
+    document.getElementById("notesList").appendChild(div);
+    document.getElementById("notesList").appendChild(br);
   })
+  console.log(list)
 }
