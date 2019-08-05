c17d06e8fd7cc84fc81864f39083668110e82857
timdavidcole <timdavidcole@gmail.com>
2019-08-05 15:23:27 +0100

    finished first specHelper

---
+++ specs/TestSpec.js
@@ -0,0 +1,5 @@
+describe('This test', function(){
+  it('does a simple test', function(){
+    expect(1).toEqual(1)
+  })
+})
--- specs/specHelper.js
+++ specs/specHelper.js
@@ -1,22 +1,19 @@
-var SpecHelper = function() {
-
-}
-
-SpecHelper.prototype.describe = function(string, fn) {
+const describe = function(string, fn) {
   console.log(string);
   fn()
 }
 
-SpecHelper.prototype.it = function(string, fn) {
+var it = function(string, fn) {
   console.log(string);
   fn()
 }
 
-SpecHelper.prototype.expect = function(variable1) {
-  function toEqual(variable2) {
-    console.log(variable1 === variable2 ? 'passed' : 'failed')
+var expect = function(exp) {
+  return new ToEqual(exp)
+}
+
+var ToEqual = function(exp) {
+  this.toEqual = function(ass) {
+    console.log(exp === ass ? 'passed' : 'failed')
   }
 }
-
-var test = new SpecHelper();
-test.expect(1).toEqual(1)
