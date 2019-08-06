92d9230a31e772b6a8163162509ebbf42c5b35c2
Kareem <kareem335@yahoo.co.uk>
2019-08-06 11:44:55 +0100

    notes are now clickable

--- notes.js
+++ notes.js
@@ -9,11 +9,11 @@ NoteList.prototype.getNoteList = function() {
 NoteList.prototype.addNote = function(content) {
   var newNote = new Note(document.getElementById('newNoteText').value)
   this.noteList.push(newNote)
-  this.updateNoteList()
+  this.updateNoteList(newNote)
   document.getElementById('newNoteText').value = '';
 }
 
-NoteList.prototype.updateNoteList = function() {
+NoteList.prototype.updateNoteList = function(note) {
   var list = document.getElementById("notesList")
   if (list.childNodes.length > 0) {
     while (list.firstChild) {
@@ -21,11 +21,15 @@ NoteList.prototype.updateNoteList = function() {
     }
   }
   this.noteList.forEach(function(note, index){
+    var content = note.content
     var div = document.createElement('div');
     var br = document.createElement('br');
     div.innerHTML = note.title;
     div.setAttribute("id", "note_" + index);
     div.setAttribute("class", "note_small");
+    div.onclick = function () {
+      this.innerHTML = content
+    }
     document.getElementById("notesList").appendChild(div);
     document.getElementById("notesList").appendChild(br);
   })
--- specs/specHelper.js
+++ specs/specHelper.js
@@ -1,19 +1,23 @@
-const describe = function(string, fn) {
+var SpecHelper = function() {
+
+}
+
+SpecHelper.prototype.describe = function(string, fn) {
   console.log(string);
   fn()
 }
 
-var it = function(string, fn) {
+SpecHelper.prototype.it = function(string, fn) {
   console.log(string);
   fn()
 }
 
-var expect = function(exp) {
-  return new ToEqual(exp)
-}
-
-var ToEqual = function(exp) {
-  this.toEqual = function(ass) {
-    console.log(exp === ass ? 'passed' : 'failed')
+SpecHelper.prototype.expect = function(variable1) {
+  function toEqual: (variable2) {
+    console.log(variable1 === variable2 ? 'passed' : 'failed')
   }
 }
+
+var test = new SpecHelper();
+test.expect(1).toEqual(1)
+
